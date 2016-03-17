cbe12a2ee6cc3e44db9aa4a160198e10bcf7c317
SenpaiDev <frostbyte@ohyessenpaipleaseputit.in>
2016-03-17 17:23:36 +0100

    Initial commit

--- main/java/Senpai/YandereForge/Yandere.java
+++ main/java/Senpai/YandereForge/Yandere.java
@@ -1,7 +1,10 @@
 package Senpai.YandereForge;
 
+import net.minecraftforge.fml.common.Mod;
+
 /**
  * Created by anonc on 17.03.2016.
  */
+@Mod(name = "YandereForge", version = "1", modid = "audauid")
 public class Yandere {
 }
