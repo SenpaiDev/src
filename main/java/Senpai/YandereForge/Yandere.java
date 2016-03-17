94d1868e36ef0f736685b9985a064c0cca88478d
SenpaiDev <frostbyte@ohyessenpaipleaseputit.in>
2016-03-17 17:24:31 +0100

    Initial commit

--- main/java/Senpai/YandereForge/Yandere.java
+++ main/java/Senpai/YandereForge/Yandere.java
@@ -1,10 +1,16 @@
 package Senpai.YandereForge;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 /**
  * Created by anonc on 17.03.2016.
  */
 @Mod(name = "YandereForge", version = "1", modid = "audauid")
 public class Yandere {
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent fmlInitializationEvent){
+
+    }
 }
