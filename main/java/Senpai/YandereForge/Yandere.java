04eb169a3dc3d2fa58b1c709a90aa2c2c67a8ad7
SenpaiDev <frostbyte@ohyessenpaipleaseputit.in>
2016-03-18 11:25:49 +0000

    Finalized the modloader idea

--- main/java/Senpai/YandereForge/Yandere.java
+++ main/java/Senpai/YandereForge/Yandere.java
@@ -1,9 +1,15 @@
 package Senpai.YandereForge;
 
 import Senpai.YandereForge.Handlers.KurumiHandler;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.ForgeEventFactory;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import scala.tools.cmd.Instance;
 import scala.tools.cmd.Opt;
 import sun.security.jca.GetInstance;
@@ -19,11 +25,17 @@ public class Yandere {
     @Mod.EventHandler
     public void Preinit(FMLPreInitializationEvent fmlInitializationEvent) {
         instance = this;
+        MinecraftForge.EVENT_BUS.register(this);
+
     }
 
     @Mod.EventHandler
     public void Init(FMLInitializationEvent fmlInitializationEvent) throws InstantiationException, IllegalAccessException {
         kurumiHandler = new KurumiHandler();
+    }
+    @SubscribeEvent
+    public void tickevent (TickEvent.ClientTickEvent clientTickEvent){
+
     }
 
 }
